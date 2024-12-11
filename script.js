4143b81a4746996c72fa745b65d7a903f6246aae
sakusarkki <145440103+sakusarkki@users.noreply.github.com>
2024-12-11 23:03:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-document.getElementById('profiiliForm').addEventListener('submit', function(event) 
-{
+document.getElementById('profiiliForm').addEventListener('submit', function(event) {
     event.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
 
-    
+    // Tallentaminen voisi olla vaikka paikallisesti selaimessa (localStorage)
     localStorage.setItem('name', name);
     localStorage.setItem('email', email);
 
     alert('Profiili tallennettu!');
 });
 
-
+// Reseptihaku
 function searchRecipes() {
     let query = document.getElementById('search').value.toLowerCase();
     let recipeList = document.getElementById('recipeList');
 
+    // Esimerkkinä reseptit
+    let recipes = [
+        'Makaroonilaatikko: 400g jauhelihaa, 7dl maitoa, makaroonia, 3 kananmunaa, uuni 200 astetta, ', 
+        'Kaalikääryleet',
+        'Karjalanpiirakat'
+    ];
 
-    recipeList.innerHTML = ''; 
+    recipeList.innerHTML = ''; // Tyhjennetään aikaisemmat tulokset
 
     recipes.forEach(function(recipe) {
         if (recipe.toLowerCase().includes(query)) {
